1fcbd6f2cd08aa0c365929c2662d61031ce31e1f
jodiema <jodiemaxf@gmail.com>
2019-05-11 20:52:34 -0700

    added solenoid code

--- Motor_test/Motor_test.ino
+++ Motor_test/Motor_test.ino
@@ -21,43 +21,43 @@ const int M4 = 7; ///<Motor4 Direction
 
 void M1_advance(char Speed) ///<Motor1 Advance
 {
- digitalWrite(M1,HIGH);
+ digitalWrite(M1,LOW);
  analogWrite(E1,Speed);
 }
 void M2_advance(char Speed) ///<Motor2 Advance
 {
- digitalWrite(M2,HIGH);
+ digitalWrite(M2,LOW);
  analogWrite(E2,Speed);
 }
 void M3_advance(char Speed) ///<Motor3 Advance
 {
- digitalWrite(M3,HIGH);
+ digitalWrite(M3,LOW);
  analogWrite(E3,Speed);
 }
 void M4_advance(char Speed) ///<Motor4 Advance
 {
- digitalWrite(M4,HIGH);
+ digitalWrite(M4,LOW);
  analogWrite(E4,Speed);
 }
 
 void M1_back(char Speed) ///<Motor1 Back off
 {
- digitalWrite(M1,LOW);
+ digitalWrite(M1,HIGH);
  analogWrite(E1,Speed);
 }
 void M2_back(char Speed) ///<Motor2 Back off
 {
- digitalWrite(M2,LOW);
+ digitalWrite(M2,HIGH);
  analogWrite(E2,Speed);
 }
 void M3_back(char Speed) ///<Motor3 Back off
 {
- digitalWrite(M3,LOW);
+ digitalWrite(M3,HIGH);
  analogWrite(E3,Speed);
 }
 void M4_back(char Speed) ///<Motor4 Back off
 {
- digitalWrite(M4,LOW);
+ digitalWrite(M4,HIGH);
  analogWrite(E4,Speed);
 }
 
@@ -72,7 +72,7 @@ void setup() {
   pinMode(A3, INPUT_PULLUP);
   pinMode(A4, INPUT_PULLUP);
   pinMode(A0, INPUT_PULLUP);
-
+  pinMode(A1, INPUT_PULLUP);
 
 /**
   for(int i=3;i<9;i++)
@@ -83,10 +83,10 @@ void setup() {
 }
 
 void forward(){
-   M1_advance(50);
-   M2_advance(50);
-   M3_advance(50);
-   M4_advance(50); 
+   M1_advance(40);
+   M2_advance(40);
+   M3_advance(40);
+   M4_advance(40); 
 }
 void left(){
   M1_advance(25);
@@ -103,39 +103,55 @@ void right(){
 }
 
 void turn90(){
-  
-  while
-   M1_back(100);
-   M2_advance(100);
-   M3_back(100);
-   M4_advance (100); 
+  unsigned long starttime = millis();
+  unsigned long endtime = starttime;
+  while ((endtime - starttime) <= 1100) // do this loop for up to 1000mS
+  {
+    Serial.println("Inside turn90");
+    // code here
+    M1_back(100);
+    M2_advance(100);
+    M3_back(100);
+    M4_advance (100);
+    endtime = millis();
+  }
+
+  for (int i = 0; i < 50; i++) {
+    Serial.println("Moving forward in turn90");
+    forward();
+  }
 }
 
 void loop() {
 
-/*delay(2000); ///<Delay 2S
-M1_back(100);
-M2_back(100);
-M3_back(100);
-M4_back(100);
-delay(2000); ///<Delay 2S */
-double a3read = analogRead(A3)/204.6;
-double a4read = analogRead(A4)/204.6;
-double a0read = analogRead(A0)/204.6;
-   if(a0read > 4.0){
-      if(a3read > 4.0 && a4read > 4.0){
-         forward();
-      }
-      else if( a3read < 4.0){
-         left();
-     }
-     else{
-       right ();
-     }
-   }
-   else{
-      turn90();
-   }
+  /*delay(2000); ///<Delay 2S
+  M1_back(100);
+  M2_back(100);
+  M3_back(100);
+  M4_back(100);
+  delay(2000); ///<Delay 2S */
+  double a3read = analogRead(A3)/204.6;
+  double a4read = analogRead(A4)/204.6;
+  double a0read = analogRead(A0)/204.6;
+  double a1read = analogRead(A1)/204.6;
+  Serial.println(a1read);
+  if (a1read > 4.0)
+  {
+    turn90();
+  }
+  else if(a3read > 4.0 && a4read > 4.0)
+  {
+     forward();
+  }
+  else if(a3read < 4.0)
+  {
+     left();
+  }
+  else
+  {
+    right();
+  }
+   
 }
 
 
